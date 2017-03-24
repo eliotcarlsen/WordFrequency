6a77b0852d012ae6b4b06bbfbd3f278599c5ce03
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-03-24 12:39:58 -0700

    add css styling

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,12 +6,11 @@
             $result_array = array();
             $lowercase_word_input = strtolower($word_input);
             $lowercase_sentence = strtolower($sentence_input);
-            $punctuation_array = array(",","!","@","#","$","^","&","*","(",")","-","=","+","[","]","{","}","|",":",";","'",'"',"<",">",".","?","/","~","_");
+            $punctuation_array = array(",","!","@","#","$","^","&","*","(",")","-","=","+","[","]","{","}","|",":",";","'",'"',"<",">",".","?","/","~","_","%");
             $remove_punctuation_sentence = str_replace($punctuation_array, " ", $lowercase_sentence);
-
             $lowercase_sentence_array = explode(" ", $remove_punctuation_sentence);
 
-
+            
             foreach ($lowercase_sentence_array as $word)
             {
                 if ($word == $lowercase_word_input)
