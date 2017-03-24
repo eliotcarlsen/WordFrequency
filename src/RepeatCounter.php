315be196aea5c819c45c2417f0800366bff495c8
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-03-24 11:55:08 -0700

    add validation for symbols

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,7 +6,19 @@
             $result_array = array();
             $lowercase_word_input = strtolower($word_input);
             $lowercase_sentence = strtolower($sentence_input);
-            $lowercase_sentence_array = explode(" ", $lowercase_sentence);
+            $remove_punctuation_sentence = str_replace(",", " ", $lowercase_sentence);
+            $remove_punctuation_sentence2 = str_replace(":", " ", $remove_punctuation_sentence);
+            $remove_punctuation_sentence3 = str_replace(";", " ", $remove_punctuation_sentence2);
+            $remove_punctuation_sentence4 = str_replace("!", " ", $remove_punctuation_sentence3);
+            $remove_punctuation_sentence5 = str_replace('"', " ", $remove_punctuation_sentence4);
+            $remove_punctuation_sentence6 = str_replace("'", " ", $remove_punctuation_sentence5);
+            $remove_punctuation_sentence7 = str_replace("?", " ", $remove_punctuation_sentence6);
+            $remove_punctuation_sentence8 = str_replace("$", " ", $remove_punctuation_sentence7);
+            $remove_punctuation_sentence9 = str_replace("#", " ", $remove_punctuation_sentence8);
+            $remove_punctuation_sentence10 = str_replace("@", " ", $remove_punctuation_sentence9);
+            $remove_punctuation_sentence11 = str_replace("%", " ", $remove_punctuation_sentence10);
+            $lowercase_sentence_array = explode(" ", $remove_punctuation_sentence11);
+
 
             foreach ($lowercase_sentence_array as $word)
             {
